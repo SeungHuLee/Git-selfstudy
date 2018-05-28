bcad5b0a1c21aa047bc8bad4dea48020128418c7
SeungHuLee <gadlsh12081208@gmail.com>
2018-05-28 20:54:09 +0900

    add getMinimum, Maximum, Average functions

--- GradeBook.h
+++ GradeBook.h
@@ -24,6 +24,49 @@ public:
 		std::cout << "Welcome to the grade book for\n" << getCourseName() << "!" << std::endl;
 	}
 
+	void processGrades() const {
+		outputGrades();
+
+		std::cout << std::setprecision(2) << std::fixed;
+		std::cout << "\nClass average is " << getAverage() << std::endl;
+
+		std::cout << "Lowest grade is " << getMinimum() << "\nHighest grade is " << getMaximum() << std::endl;
+
+		outputBarChart();
+	}
+
+	int getMinimum() const {
+		int lowGrade{ 100 };
+
+		for (int grade : grades) {
+			if (grade < lowGrade) {
+				lowGrade = grade;
+			}
+		}
+		return lowGrade;
+	}
+
+	int getMaximum() const {
+		int highGrade{ 0 };
+
+		for (int grade : grades) {
+			if (grade > highGrade) {
+				highGrade = grade;
+			}
+		}
+		return highGrade;
+	}
+
+	double getAverage() const {
+		int total{ 0 };
+
+		for (int grade : grades) {
+			total += grade;
+		}
+
+		return static_cast<double>(total) / grades.size();
+	}
+	
 private:
 	std::string courseName;
 	std::array<int, students>grades;
